dc46c34147a3aef92d20cd69984872ed296e0b02
Sebastian Boldt <self.dealloc@googlemail.com>
2018-11-15 21:31:08 +0100

    added deprecation attributes

---
+++ Jelly/Classes/public/Deprecation.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+@available(*, deprecated: 2.0.0, renamed: "SlidePresentation")
+public typealias JellyShiftInPresentation = SlidePresentation
+
+@available(*, deprecated: 2.0.0, renamed: "CoverPresentation")
+public typealias JellySlideInPresentation = CoverPresentation
+
+@available(*, deprecated: 2.0.0, renamed: "FadePresentation")
+public typealias JellyFadeInPresentation = FadePresentation
+
+@available(*, deprecated: 2.0.0, renamed: "Animator")
+public typealias JellyAnimator = Animator
+
+@available(*, deprecated: 2.0.0, renamed: "Spring")
+public typealias Jellyness = Spring
